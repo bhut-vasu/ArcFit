888b6d0c3b5bcfbdf706b8c87800c81b887a7465
Bhut Vasu <87259010+bhut-vasu@users.noreply.github.com>
2024-01-11 00:59:39 +0530

    Update Contact.jsx

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -26,7 +26,7 @@ const Contact = () => {
       return false;
     }
 
-    await fetch("http://localhost:5000/", {
+    await fetch("https://arc-fit-server.vercel.app/", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
